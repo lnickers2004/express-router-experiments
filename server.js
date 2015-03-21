7cb4e06930f07a21689b72f16c03287424698ed4
Larry Nickerson <lnickers@austin.rr.com>
2015-03-21 02:43:04 -0500

    updated ignores and changed server port to 3000

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 
 var express = require('express');
 var app     = express();
-var port    = 	process.env.PORT || 8080;
+var port    = 	process.env.PORT || 3000git st;
 
 // ROUTES
 // ==============================================
